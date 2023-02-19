e714f20704ac158b9a992c064e8baa29b353270f
anshu0202 <anshu.verma62074@gmail.com>
2023-02-20 01:19:24 +0530

    multer changed

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import bodyParser from 'body-parser';
 import Route from './routes/route.js';
 import Connection from './database/db.js';
 import cors from 'cors';
+// import {Server} from "socket.io";
 
 const app=express();
 app.use(cors());
@@ -16,3 +17,55 @@ const PORT=5000;
 app.listen(PORT,()=>{
     console.log("server is started at PORT no : ",PORT)
 })
+
+
+
+
+//socket code 
+
+// const io = new Server(9005, {
+//     cors:{
+//         origin:'http://localhost:3000',
+//         // credentials:true
+//     },
+// })
+
+// let users=[];
+// const addUser= (userData, socketId)=>{
+//     !users.some((user) => user.sub === userData.sub) &&  users.push({... userData, socketId});
+// }
+
+
+// const getUser=(userId)=>{
+
+//     return users.find(user => user.sub === userId); 
+
+// }
+
+// io.on('connection',(socket) => {
+//    // socket is an object which has all the information which come from the frontend
+
+//     console.log("user connected")
+//     socket.on("addUsers",(userData) => {
+//         addUser(userData, socket.id);
+//         // it has been used to send data to frontend from the backend
+//         io.emit("getUsers",users)
+//     });
+
+//     //
+//     socket.on('sendMessage',(data) =>{
+
+//         console.log("data is ", data);
+//         // if(data){
+//             const user = getUser(data.receiverId);
+//             io.to(user.socketId).emit('getMessage', data);
+
+//         // }
+//         // else{
+//         //     console.log("error is socket")
+//         // }
+   
+
+//     })
+// })
+
